5f4a831c6515799bdcc7aaaeb9f99124a8872f93
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-17 19:56:32 +1200

    Add files via upload
    Make sure that when the question 'Shape:' appears the user will need to input something...it can not be blank

---
+++ 01_not_blank.py
@@ -0,0 +1,22 @@
+'''
+component 1 - name not blank
+v1 - If user does not enter something then repeat question
+'''
+
+# Functions go here
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response !="":
+            return response
+        else:
+            print("Sorry, this can't be blank. "
+                  "Please enter a shape name!")
+
+
+# Main routine goes here
+shape = not_blank("Shape: ")
