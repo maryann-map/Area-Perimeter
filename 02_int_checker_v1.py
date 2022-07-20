6ee1d9d939dea57c833bc17c003b7113ef375c76
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-20 15:32:50 +1200

    Add files via upload
    Check that valid integers are used

---
+++ 02_int_checker_v1.py
@@ -0,0 +1,27 @@
+'''
+component 2 - integer checker function
+v1 - checking to see that the function is called, returns value
+'''
+
+#Functions
+
+def int_check():
+
+    error = ("Please enter a valid integer")
+
+    valid = False
+    while not valid:
+
+        try:
+            length = int(input("Length: "))
+            if length <= 0:
+                print(error)
+            else:
+                return length
+
+        #if an integer is not entered, display error message
+        except ValueError:
+            print(error)
+
+# main routine
+int_check()
