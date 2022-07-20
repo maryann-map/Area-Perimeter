efe55a6a167aab89bf46db3da0db2bcb049cca36
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-20 16:09:05 +1200

    Add files via upload
    Calculating perimeter of shape

---
+++ 05_calculate_perimeter_v1.py
@@ -0,0 +1,18 @@
+'''
+component 5 - calculating perimeter of shape
+'''
+
+# Ask user for sides of square
+length = int(input("Length: "))
+width = int(input("Width: "))
+
+# Calculate perimeter by adding multiplying each side by 2 and then add them together
+perimeter = (2*length) + (2*width)
+
+# Show answer
+print("Area: {}".format(perimeter))
+
+
+
+
+
