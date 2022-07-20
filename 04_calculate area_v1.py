27d0497a476ee8f9582da9a3d036ff722193e9cb
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-20 15:45:54 +1200

    Add files via upload
    Calculating Area

---
+++ 04_calculate area_v1.py
@@ -0,0 +1,18 @@
+'''
+component 4 - calculating area of sh
+'''
+
+# Ask user for sides of square
+length = int(input("Length: "))
+width = int(input("Width: "))
+
+# Calculate are by multiplying side1 and side2
+area = length * width
+
+# Show answer
+print("Area: {}".format(area))
+
+
+
+
+
