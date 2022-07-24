2d70daeb8ebecaaf59bf683a2b8f188da58d49eb
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-24 22:06:47 +1200

    Add files via upload
    08_summary_v1.py = creating lists to store data for the shapes chosen and their areas and perimeters.
    08_summary_v2.py = display it in a table

---
+++ 08_summary_v1.py
@@ -0,0 +1,17 @@
+'''
+component 8 version 1 - create list to store shape name, area & perimeter of each shape
+'''
+
+# List to store each shape that was chosen by user
+all_shapes = ['Circle', 'Square', 'Triangle', 'Parallelogram', 'Rectangle']
+
+# List to store the area of each shape
+area = []
+
+# List to store the perimeter of each shape
+perimeter = []
+
+answers_list = [area, perimeter]
+
+print(all_shapes)
+
---
+++ 08_summary_v2.py
@@ -0,0 +1,21 @@
+'''
+component 8 version 2 - Adding in data frame
+'''
+
+# All data in the lists are for testing purposes
+
+# Import Panda package
+import pandas as pd
+
+# List to store all the chosen shapes
+all_shapes = ['Circle', 'Square', 'Triangle', 'Parallelogram', 'Rectangle']
+
+# List to store the area of each shape
+area = ['3', '4', '5', '6', '7']
+
+# List to store the perimeter of each shape
+perimeter = ['7', '6', '5', '4', '3']
+
+df = pd.DataFrame(list(zip(all_shapes, area, perimeter)), columns=['Shape', 'Area', 'Perimeter'])
+
+print(df)
