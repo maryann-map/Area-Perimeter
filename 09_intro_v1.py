465de307b49a0388c54ad97c59770db75ec6f1f2
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-07-25 02:34:59 +1200

    Add files via upload
    Introduction to programme

---
+++ 09_intro_v1.py
@@ -0,0 +1,10 @@
+'''
+component 9 version 1 - Welcome statement & Explain what the programme does
+'''
+
+# Welcome Statement & Title of Programme
+print()
+print("Welcome to the Shape Calculator!!!")
+
+# Explain what the programme does
+print("This programme can calculate the AREA and PERIMETER of 5 different shapes")
---
+++ 09_intro_v2.py
@@ -0,0 +1,30 @@
+'''
+component 9 version 2 - How the programme works & what to be aware of
+'''
+
+# Welcome Statement & Title of Programme
+print()
+print("Welcome to the Shape Calculator!!!")
+
+# Explain what the programme does
+print("This programme can calculate the AREA and PERIMETER of 5 different shapes")
+
+# How programme works
+print("This is how it works:")
+print("1. Choose shape from one of the available shapes")
+print("2. Enter the dimensions of shape (eg: length & width")
+print("3. Enter your area and perimeter answer in 2 decimal place")
+
+# What to be aware of
+print("If you type something that is not one of the choices, then the question will repeat")
+print("If you get the answer wrong, it will let you know")
+print("Choosing shape: You can enter the first letter, number (1-5) or the first 3 letters")
+
+# Available shapes
+print("These are the available shapes:")
+print("1 = Circle | 2 = Square | 3 = Rectangle | 4 = Triangle | 5 = Parallelogram")
+
+
+
+
+
